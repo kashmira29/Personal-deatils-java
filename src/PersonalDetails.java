749316799d6471e004f6e1b9defff9e8dd3fab3c
kashmira29 <kashmirapandar@gmail.com>
2019-07-29 01:03:13 +0100

    first commit

---
+++ src/PersonalDetails.java
@@ -0,0 +1,25 @@
+public class PersonalDetails {
+    public static void main(String args[]) {
+
+        String name = "Kashmira Pandar";
+        System.out.println("Name : " + name);
+        int age = 35;
+        System.out.println("Age : " + age);
+        int house_number = 6;
+        System.out.println("House Number : " + house_number);
+        String postcode = "HA1_4EE";
+        System.out.println("Post Code :"+ postcode);
+        String city = "Harrow";
+        System.out.println("City : " + city);
+        String mobile = "0742950829";
+        System.out.println("Mobile : " + mobile);
+        int salary = 1500;
+        System.out.println("salary :" + salary);
+
+
+
+    }
+
+
+}
+
